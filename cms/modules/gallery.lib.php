1c8a37eee3e89bbab9356d4d26d6cb957551a623
Abhishek Srivastava <i.abhi27@gmail.com>
2010-06-12 11:27:54 +0000

    Fixed the gallery module

--- cms/modules/gallery.lib.php
+++ cms/modules/gallery.lib.php
@@ -61,7 +61,7 @@ JS;
 		$gallQuery = "SELECT * from `gallery_name` where `page_modulecomponentid`=$this->moduleComponentId";
 		$gallResult = mysql_query($gallQuery);
 		$row = mysql_fetch_assoc($gallResult);
-		$content .= "<style type=\"text/css\">.pragyangallery{margin:10px;padding:10px}</style><h2><center>{$row['gallery_name']}</center></h2><br/><center><h3>{$row['gallery_desc']}</center></h3>";
+		$content .= "<h2><center>{$row['gallery_name']}</center></h2><br/><center><h3>{$row['gallery_desc']}</center></h3>";
 		include_once ("$sourceFolder/" . 'upload.lib.php');
 		$arr = getUploadedFiles($this->moduleComponentId, 'gallery');
 
@@ -99,21 +99,21 @@ JS;
 		$arr = getUploadedFiles($moduleComponentId, 'gallery');
 		if (isset ($_POST['btnDeleteImage']) && isset ($_POST['imagename']) && $_POST['imagename'] != '') {
 			deleteFile($moduleComponentId, 'gallery', $_POST['imagename']);
-			$gallQuery = "DELETE FROM `gallery_pics` WHERE `upload_filename`='{$_POST['imagename']}'";
+			$gallQuery = "DELETE FROM `gallery_pics` WHERE `upload_filename`='".escape($_POST['imagename'])."'";
 			$gallResult = mysql_query($gallQuery);
 		} 
 		else if (isset ($_POST['btnEditComment']) && isset ($_POST['imagename']) && $_POST['imagename'] != '') {
-			$imageName =  $_POST['imagename'];
-			$comment = $_POST['desc'];
+			$imageName =  escape($_POST['imagename']);
+			$comment = escape($_POST['desc']);
 			$gallQuery = "UPDATE `gallery_pics` SET `gallery_filecomment`=\"$comment\" WHERE `upload_filename`=\"$imageName\"";
 			$gallResult = mysql_query($gallQuery);
 		}
 		if (isset ($_POST['btnEditGallname']) && isset ($_POST['gallName']) && isset ($_POST['gallDesc']) && $_POST['gallName'] != '' && $_POST['gallDesc'] != '') {
-			$gallQuery = "UPDATE `gallery_name` SET `gallery_name`='{$_POST['gallName']}',`gallery_desc`='{$_POST['gallDesc']}' WHERE `page_modulecomponentid`=$moduleComponentId";
+			$gallQuery = "UPDATE `gallery_name` SET `gallery_name`='".escape($_POST['gallName'])."',`gallery_desc`='".escape($_POST['gallDesc'])."' WHERE `page_modulecomponentid`=$moduleComponentId";
 			$gallResult = mysql_query($gallQuery);
 		}
 
-		$content2 .= getFileUploadForm($this->moduleComponentId, "gallery", './+edit', 10000000, 5);
+		$content2 = getFileUploadForm($this->moduleComponentId, "gallery", './+edit', 10000000, 5);
 		$allowableTypes = array (
 			'jpeg',
 			'jpg',
@@ -124,7 +124,7 @@ JS;
 		$uploadSuccess = submitFileUploadForm($this->moduleComponentId, "gallery", $this->userId, false, $allowableTypes);
 		if (is_array($uploadSuccess) && isset ($uploadSuccess[0])) {
 			for($i=0;$i<count($uploadSuccess);$i++){
-				$gallQuery3 = "INSERT INTO `gallery_pics` (`upload_filename`, `page_modulecomponentid`, `gallery_filecomment`) VALUES('$uploadSuccess[$i]', $this->moduleComponentId, '{$_POST['desc']}')";
+				$gallQuery3 = "INSERT INTO `gallery_pics` (`upload_filename`, `page_modulecomponentid`, `gallery_filecomment`) VALUES('$uploadSuccess[$i]', $this->moduleComponentId, '".escape($_POST['desc'])."')";
 				$gallResult3 = mysql_query($gallQuery3);
 			}
 		}
@@ -133,13 +133,13 @@ JS;
 					<br /><br />
 					<form name="edit" method="POST" action="./+edit">
 					<table>
-						<tr><td>Edit gallery name and description</td></tr>
+						<tr><th colspan=2>Edit gallery name and description</th></tr>
 						<tr>
-							<td>Gallery Name</td>
+							<td>New Gallery Name</td>
 							<td><input type='text' name="gallName"></td>
 						</tr>
 						<tr>
-							<td>Gallery Description</td>
+							<td>New Gallery Description</td>
 							<td><input type='text' name="gallDesc"></td>
 						</tr>
 						<tr>
@@ -158,7 +158,7 @@ GALFORM;
 			for ($i = 0; $i < count($fileArray); $i++) {
 				$galleryFilename = $fileArray[$i]['upload_filename'];
 				$galleryComment = $fileArray[$i]['gallery_filecomment'];
-				$content2 .= "<form name=\"edit\" method=\"POST\" action=\"./+edit\">";
+				$content2 .= "<div class='galleryimagebox'><form name=\"edit\" method=\"POST\" action=\"./+edit\">";
 				$content2 .=<<<IMGFORM
 				<span style="float:left">
 					<center>
@@ -167,11 +167,11 @@ GALFORM;
 					<div class="highslide-caption" id="caption$i">$galleryComment</div>
 					<input type="hidden" name="imagename" value="$galleryFilename" />
 					<input type="text" name="desc">
-					<br/><input type="submit" name="btnEditComment" value="Edit comment">
-					<br/><input type="submit" name="btnDeleteImage" value="Delete" />
+					<br/><input type="submit" name="btnEditComment" value="Update comment">
+					<input type="submit" name="btnDeleteImage" value="Delete" />
 				</span>
 IMGFORM;
-				$content2 .= "</form>";
+				$content2 .= "</form></div>";
 			}
 		}
 		return $content2;
