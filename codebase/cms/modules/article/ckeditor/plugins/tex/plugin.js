255aee93e3805297ca8aac483aca9156f718e026
Abhishek Srivastava <i.abhi27@gmail.com>
2010-07-24 19:19:02 +0000

    Added tex plugin for ckEditor in Pragyan CMS

---
+++ codebase/cms/modules/article/ckeditor/plugins/tex/plugin.js
@@ -0,0 +1,21 @@
+var a= {  
+        exec:function(editor){  
+			var theSelectedText = editor.getSelection().getNative();
+			var FormattedText = '[tex]'+theSelectedText+'[/tex]';
+			editor.insertHtml(FormattedText);
+        }  
+    },
+b='tex';  
+CKEDITOR.plugins.add(b,{  
+        init:function(editor){  
+            editor.addCommand(b,a);  
+            editor.ui.addButton('tex',{  
+                label:'Convert to Image using Tex',   
+                icon: this.path + 'tex.png',  
+                command:b  
+            });  
+        }  
+    });      
+
+  
+CKEDITOR.config.syrinx_siteBase = "";  
