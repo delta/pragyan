08a3c8b9b09e894accba31e1ef474b0f45c4f33d
Abhishek Srivastava <i.abhi27@gmail.com>
2010-08-17 11:11:25 +0000

    Added Google maps plugin for ckEditor

--- codebase/cms/modules/article/ckeditor/config.js
+++ codebase/cms/modules/article/ckeditor/config.js
@@ -8,6 +8,8 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.extraPlugins='tex';
+	config.extraPlugins='tex,googlemaps';
 	config.toolbar_Full.push(['tex']);
+
+	config.toolbar_Full.push(['googlemaps']);
 };
