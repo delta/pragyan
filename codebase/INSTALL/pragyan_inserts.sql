343ab22b84068a8da2ec5c450cf917c499e45e38
balanivash <balanivash@gmail.com>
2010-12-10 18:45:59 +0000

    Adding News widget

--- codebase/INSTALL/pragyan_inserts.sql
+++ codebase/INSTALL/pragyan_inserts.sql
@@ -295,7 +295,8 @@ INSERT IGNORE INTO `pragyanV3_userpageperm` (`perm_type`, `page_id`, `usergroup_
 
 INSERT IGNORE INTO `pragyanV3_widgetsinfo` (`widget_id`, `widget_name`, `widget_classname`, `widget_description`, `widget_version`, `widget_author`, `widget_foldername`) VALUES
 (1, 'Server Date and Time', 'serverDateTime', 'Display the current date and time in the website', '0.01', 'Abhishek Shrivastava', 'server_date_time'),
-(2, 'Count Down', 'count_down', 'Display the countdown to a given date', '0.01', 'Balanivash', 'count_down');
+(2, 'Count Down', 'count_down', 'Display the countdown to a given date', '0.01', 'Balanivash', 'count_down'),
+(3, 'News', 'news', 'Display the news/updates', '0.01', 'Balanivash', 'news');
 
 
 --
