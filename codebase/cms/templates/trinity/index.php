9576d321d2aed8001fea34182bb72b219c70b8ce
Boopathi Rajaa <boopathi@live.com>
2010-12-11 10:20:54 +0000

    Changed clock date in trinity template

--- codebase/cms/templates/trinity/index.php
+++ codebase/cms/templates/trinity/index.php
@@ -26,7 +26,7 @@
     <script type="text/javascript" src="<?php echo $TEMPLATEBROWSERPATH; ?>/scripts/jquery.ticker.min.js"></script>
     <script type="text/javascript" src="<?php echo $TEMPLATEBROWSERPATH; ?>/scripts/script.js"></script>
     <script language="JavaScript">
-			TargetDate = "02/01/2011 00:00 AM";
+			TargetDate = "02/17/2011 00:00 AM";
 			BackColor = "none";
 			ForeColor = "white";
 			CountActive = true;
