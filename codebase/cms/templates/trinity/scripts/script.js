c2641e74d97e0e455793c5c62e80781929dd33b5
Boopathi Rajaa <boopathi@live.com>
2011-01-10 04:31:11 +0000

    Trinity Improvements

--- codebase/cms/templates/trinity/scripts/script.js
+++ codebase/cms/templates/trinity/scripts/script.js
@@ -24,20 +24,27 @@ $(function() {
 	/*
 	 * Hover Function for Menu
 	 */
-	$("ul.topnav li").hover(function(){
-	 	$(this).children("ul.subnav").fadeIn();
+	$("ul.topnav > li").hover(function(){
+	 	$(this).children("ul.subnav").css({display:"block",opacity:0,left: 50}).animate({left: 0, opacity:1}, 250);
 	},function() {
 		$(this).find("ul.subnav").css({display: 'none'});
 	});
 	
-	$("ul.topnav li").bind({
+	$("ul.topnav > li").bind({
 		mouseenter: function() {
-			$(this).animate({left: "+=2"}, 50);
+			$(this).animate({paddingLeft: 10}, 75);
 		},
 		mouseleave: function() {
-			$(this).animate({left: "-=2"}, 50);
+			$(this).animate({paddingLeft: 0}, 100);
 		}
 	});
+	
+	$("ul.subnav > li").hover(function(){
+	 	$(this).children("ul.subnav").fadeIn(100);
+	},function() {
+		$(this).find("ul.subnav").css({display: 'none'});
+	});
+	
 	///Write in StatusBar - "To"
 	$("a").bind({
 		mouseenter: function(){
@@ -49,38 +56,55 @@ $(function() {
 	});
 	
 	///Login Form
-	$("a.cms-actionlogin").hover(function() {
-		$("a.cms-actionlogin").css({
-			backgroundColor : "white",
-			border: "solid 1px #345",
-			color : "black"
-		});///<Any Changes in menu.css will now automatically reflect here. Should be done manually
+	$("a.cms-actionlogin").click(function() {
 		
 		///User-friendly and safety - password clear
-		$("#hc_loginform").fadeIn(100).find("#user_email").focus().end().find("#user_password").attr("value","");
+		$("#hc_loginform").css({display:"block", top:-200}).animate({top:0}, 100).find("#user_email").focus().end().find("#user_password").attr("value","");
 		
 		///Display form when mouse over the table.
 		$("#hc_loginform").hover(function(){
-				$(this).css("display","block");
-				$("a.cms-actionlogin").css({
-					backgroundColor : "white",
-					border: "solid 1px #345",
-					color : "black"
-					});
+				$(this).css({display: "block"});
+				
 			}, function(){
-				$(this).fadeOut();//css("display","none");
-				$("a.cms-actionlogin").css({
-					background : "none",
-					border: "none",
-					color : "white"
-				});
+				$(this).slideUp(100);//css("display","none");
+				
 		});
+		return false;
 	});
 	
 	/**
 	 * Disable navigation to login form page
 	 */
 	$("a.cms-actionlogin").bind({click: function(e){ e.preventDefault(); return false; }});
+	
+	/*
+	 * Extended Header
+	 */
+	var isClosed = true;
+	var extTarget = $("#exthead"); //to reduce reflow
+	$(".extendHeadLink a").bind({
+		mouseenter: function() {
+			extTarget.css({top: -3});
+		},
+		mouseleave: function() {
+			if(isClosed)
+				extTarget.css({top: 0});
+		}
+	}).click(function() {
+		if(isClosed) {
+			isClosed = false;
+			extTarget.animate({height: 300}, 400,function(){$(this).children(".extendedContainer").fadeIn(50);});
+			var path= templateBrowserPath + "/../common/icons/16x16/actions/go-up.png"
+			$(this).find("img").attr("src", path);
+		}
+		else {
+			isClosed = true;
+			extTarget.children(".extendedContainer").fadeOut(50, function(){$(this).parent().animate({height: 0}, 300);});
+			var path= templateBrowserPath + "/../common/icons/16x16/actions/go-down.png"
+			$(this).find("img").attr("src", path);
+		}
+		return false;
+	});	
 	
 	/**
 	 * Profile Menu
@@ -101,30 +125,39 @@ $(function() {
 	 */
 
         $('#openidLogin').bind("click",function(e){
-        				$("#hc_loginform").animate({width: 500});
-                $("#openid_form *").fadeIn(100,function(){
-                        $("hc_loginform fieldset").fadeOut();
-		    				});
-                $("#pragyan_loginform *").fadeOut(100,function(){
-                        $("hc_loginform fieldset").fadeOut();
-		    				});
+			$("#hc_loginform").animate({width: 450, height: 50}, function() {
+				$(this).animate({height: 200});
+				$("#openid_form *").fadeIn(100,function(){
+		        	$("hc_loginform fieldset").fadeOut();
+				});
+			
+			});
+    		
+    		$("#pragyan_loginform *").fadeOut(100,function(){
+            	$("hc_loginform fieldset").fadeOut();
+			});
 
-                e.preventDefault();
-                return false;
+		    e.preventDefault();
+		    return false;
 	    });
 
         $('#pragyanLogin').bind("click",function(e){
-                $("#pragyan_loginform *").fadeIn(100,function(){
-                        $("hc_loginform fieldset").fadeOut();
-		    				});
-                $("#openid_form *").fadeOut(100,function(){
-                        $("hc_loginform fieldset").fadeOut();
-		    				});
+        	$("#hc_loginform").animate({width: 270});
+            $("#pragyan_loginform *").fadeIn(100,function(){
+		        $("hc_loginform fieldset").fadeOut();
+			});
+	        $("#openid_form *").fadeOut(100,function(){
+	        	$("hc_loginform fieldset").fadeOut();
+	    	});
 
-                e.preventDefault();
-                return false;
+            e.preventDefault();
+            return false;
 	    });
 	
+	
+	/*
+	Title Bar mouseover effect
+	*/
 	
 	///Expand on mouse click in registration field
 
