c5038cb401608535613b9a6b91ddc29ff9948dd3
Boopathi Rajaa <boopathi@live.com>
2011-08-14 04:01:25 +0530

    enabled feature user:username to redirect to profile

---
+++ cms/userprofile.lib.php
@@ -0,0 +1,17 @@
+<?php
+if(!defined('__PRAGYAN_CMS'))
+{ 
+	header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+	echo "<h1>403 Forbidden<h1><h4>You are not authorized to access the page.</h4>";
+	echo '<hr/>'.$_SERVER['SERVER_SIGNATURE'];
+	exit(1);
+}
+/**
+ * @package pragyan
+ * @author Boopathi Rajaa
+ * @copyright (c) 2011 Pragyan Team
+ * @license http://www.gnu.org/licenses/ GNU Public License
+ * For more details, see README
+ */
+
+echo "adfads";
